6947192e597a5478d75850eeecd557116020cac2
Arun Purang <arun@bluedot.com.au>
2015-02-20 18:46:35 +1100

    Added new endpoint for zones

---
+++ zone/GetAllZones.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Balendran on 24/10/2014.
+ * Get a list of zones by Api Key or Customer API key
+ */
+
+
+var request  = require('request');
+
+
+var  apiKey         = '066ce100-2677-11e4-9331-bc305bf60831';
+var  customerApiKey = '50e40a61-79d3-11e4-b239-a0481cdba483';
+
+request.get ( 'https://api.bluedotinnovation.com/1/zones?customerApiKey=' +customerApiKey,
+    function (error, response) {
+        if (error) {
+            console.log(error);
+        }
+        console.log(response.body);
+    }
+);
